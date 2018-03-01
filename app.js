0a38cf65633396460fa62cda2ba80701f9e2937d
Jakub Kosior <jakubjkosior@gmail.com>
2018-03-01 23:57:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+io.on("connection", client =>{
+
+    client.on("join", name =>{
+        client.nickname = name;
+        console.log(`${name} joined`);
+    });
+
+    client.on("messages", data => {
+        let emitedData = {
+            name: client.nickname,
+            message: data
+        };
+        client.broadcast.emit("messages", emitedData);
+        client.emit("messages", emitedData);
+    });
+
+});
+
+app.use("/static", express.static(`${__dirname}`));
+
+app.get("/", (req,res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+server.listen(8080);
